571cc2dcf06b0ea070d80ece871e11e6893a0094
Umaaz <b.w.donnelly1@googlemail.com>
2011-05-09 16:49:49 +0100

    adjusted FilmMap to allow for larger fields, due to the indexing issue.

--- src/MediaApp/Mappings/FilmMap.cs
+++ src/MediaApp/Mappings/FilmMap.cs
@@ -13,11 +13,11 @@ namespace MediaApp.Mappings
             Map(x => x.Title).Length(300);
             Map(x => x.RunTime);
             Map(x => x.ReleaseYear).Length(4);
-            Map(x => x.FilmPath);
+            Map(x => x.FilmPath).Length(1000);
             Map(x => x.Synopsis).Length(4000);
             Map(x => x.OnExternalMedia);
             Map(x => x.TrailerLink).Length(263);
-            Map(x => x.Keywords);
+            Map(x => x.Keywords).Length(4000);
 
             HasMany(x => x.Writers).AsBag().Cascade.All();
             HasMany(x => x.Director).AsBag().Cascade.All();
@@ -30,10 +30,10 @@ namespace MediaApp.Mappings
                 .AsBag()
                 .Cascade.All();
 
-            Map(x => x.DirectorIndexing);
-            Map(x => x.CharIndexing);
-            Map(x => x.GenreIndexing);
-            Map(x => x.PersonIndexing);
+            Map(x => x.DirectorIndexing).Length(4000);
+            Map(x => x.CharIndexing).Length(4000);
+            Map(x => x.GenreIndexing).Length(4000);
+            Map(x => x.PersonIndexing).Length(4000);
         }
     }
 }
